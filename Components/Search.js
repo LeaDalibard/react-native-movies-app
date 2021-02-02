9d637050d82b6f6c1e14949dcb0dabce500b6196
Lea Dalibard <leadalibard@gmail.com>
2021-02-02 15:14:13 +0100

    Adding Stylesheet

--- Components/Search.js
+++ Components/Search.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import {View, TextInput, Button} from 'react-native'
+import {StyleSheet,View, TextInput, Button} from 'react-native'
 
 class Search extends React.Component {
     render() {
         return (
             <View style={{marginTop: 20}}>
-                <TextInput style={styles.textinput} placeholder='Movie title'/>
+                <TextInput style={[styles.textinput,{marginBottom:10}]} placeholder='Movie title'/>
                 <Button title='Search' onPress={() => {
                 }}/>
             </View>
@@ -13,14 +13,16 @@ class Search extends React.Component {
     }
 }
 
-const styles = {
-    textinput: {
-        marginLeft: 5,
-        marginRight: 5,
-        height: 50,
-        borderColor: '#000000',
-        borderWidth: 1,
-        paddingLeft: 5,
+const styles = StyleSheet.create(
+    {
+        textinput: {
+            marginLeft: 5,
+            marginRight: 5,
+            height: 50,
+            borderColor: '#000000',
+            borderWidth: 1,
+            paddingLeft: 5,
+        }
     }
-}
+)
 export default Search
