c07f3b4f4fe8844f5ee64b26e44893eb9db77ea8
Lea Dalibard <leadalibard@gmail.com>
2021-02-02 15:04:08 +0100

    Adding const styles to put style apart

--- Components/Search.js
+++ Components/Search.js
@@ -1,15 +1,26 @@
 import React from 'react'
-import { View, TextInput, Button } from 'react-native'
+import {View, TextInput, Button} from 'react-native'
 
 class Search extends React.Component {
     render() {
         return (
-            <View style={{marginTop:20}}>
-                <TextInput placeholder='Movie title'/>
-                <Button title='Search' onPress={() => {}}/>
+            <View style={{marginTop: 20}}>
+                <TextInput style={styles.textinput} placeholder='Movie title'/>
+                <Button title='Search' onPress={() => {
+                }}/>
             </View>
         )
     }
 }
 
+const styles = {
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5,
+    }
+}
 export default Search
