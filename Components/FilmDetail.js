d9e8b8044419307ecc4bc02a57e98e10ad52975a
Lea Dalibard <leadalibard@gmail.com>
2021-02-03 15:42:53 +0100

    Adding new component FilmDetail

---
+++ Components/FilmDetail.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {StyleSheet, View,Text} from "react-native-web";
+
+class FilmDetail extends React.Component{
+    render(){
+        return(
+            <View style={styles.main_container}>
+                <Text>Film Details</Text>
+            </View>
+        )
+    }
+}
+
+const styles=StyleSheet.create({
+    main_container:{
+        flex:1,
+    }
+})
+
+export default FilmDetail
