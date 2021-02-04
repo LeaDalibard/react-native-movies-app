0ffa1efecc0ef88a6e83e55de36e0db4cf152e05
Lea Dalibard <leadalibard@gmail.com>
2021-02-04 09:54:44 +0100

    Add loading method to FilmDetail

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,11 +1,31 @@
 import React from 'react'
-import {StyleSheet, View,Text} from "react-native-web";
+import ActivityIndicator, {StyleSheet, View,Text} from "react-native";
 
 class FilmDetail extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state={
+            film:undefined,
+            isLoading:true
+        }
+    }
+
+    _displayLoading() {
+        if (this.state.isLoading){
+            return(
+                <View style={styles.loading_container}>
+                    <ActivityIndicator size='large'/>
+                </View>
+            )
+        }
+    }
+
     render(){
         return(
             <View style={styles.main_container}>
-                <Text>Film Details  {this.props.navigation.state.params.idFilm}</Text>
+                <Text>Film Details </Text>
+                {this._displayLoading()}
             </View>
         )
     }
@@ -14,6 +34,15 @@ class FilmDetail extends React.Component{
 const styles=StyleSheet.create({
     main_container:{
         flex:1,
+    },
+    loading_container:{
+        position:'absolute',
+        left:0,
+        right:0,
+        top:0,
+        bottom:0,
+        alignItems:'center',
+        justifyContent:'center'
     }
 })
 
